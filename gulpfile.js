49e97d5b0f45829911b419227173d0936a398071
Alessandro Caligaris <alessandro.caligaris1991@gmail.com>
2015-08-25 10:20:36 +0200

    Directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,43 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     compass = require('gulp-for-compass'),
     htmlmin = require('gulp-htmlmin'),
-    devdir  = 'build';
+    concat  = require('gulp-concat'),
+    uglify  = require('gulp-uglify'),
+    devdir  = 'build',
+    proddir = 'prod',
+    vendorlib = './coffee/vendor/';
 
 gulp.task('default', function () {
+    gulp.src('./coffee/**/*.coffee')
+        .pipe(coffee())
+        .pipe(gulp.dest('./' + devdir + '/js/'));
+
+    gulp.src('./sass/*.scss')
+        .pipe(compass({
+            sassDir: './sass/',
+            cssDir: './' + devdir + '/css/',
+            outputStyle: 'compressed'
+        }));
+
+    gulp.src('./templates/**/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./' + devdir + '/'));
+
+    gulp.src([
+            vendorlib + 'jquery.min.js',
+            vendorlib + 'jquery.scrollToTop.min.js',
+            vendorlib + 'TweenMax.min.js',
+            vendorlib + 'underscore-min.js',
+            vendorlib + 'backbone-min.js',
+            vendorlib + 'handlebars.min.js',
+            vendorlib + 'thorax.min.js',
+        ])
+        .pipe(uglify())
+        .pipe(concat('lib.js'))
+        .pipe(gulp.dest('./' + devdir + '/js/'));
+});
+
+gulp.task('devel', function () {
     gulp.src('./coffee/**/*.coffee')
         .pipe(coffee())
         .pipe(gulp.dest('./' + devdir + '/js/'));
